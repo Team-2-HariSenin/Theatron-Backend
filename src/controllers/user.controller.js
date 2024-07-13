cd5d23da406f5a0097af4353c6b540537a16e019
“diksom” <diki_somantri@ymail.com>
2024-07-14 01:04:24 +0800

    Enhance profile edit functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,17 +19,37 @@ module.exports = {
   async updateUser(req, res) {
     try {
       const id = req.user.id;
-      const { name, email, password } = req.body;
+      const { name, email, oldPassword, newPassword } = req.body;
       const user = await User.findOne({ where: { id } });
-      if (user) {
-        const hashedPassword = password
-          ? await bcrypt.hash(password, 10)
-          : user.password;
-        await user.update({ name, email, password: hashedPassword });
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: "User not found" });
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
       }
+
+      if (oldPassword && newPassword) {
+        // Verifikasi oldPassword
+        const isMatch = await bcrypt.compare(oldPassword, user.password);
+        if (!isMatch) {
+          return res.status(400).json({ message: "Old password is incorrect" });
+        }
+
+        // Hash newPassword
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+      }
+
+      // Update name and email
+      if (name) user.name = name;
+      if (email) user.email = email;
+
+      await user.save();
+
+      const updatedUser = await User.findOne({
+        where: { id },
+        attributes: { exclude: ["password"] },
+      });
+
+      res.status(200).json(updatedUser);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { verifyToken } = require("../middlewares/auth");
-
+const { validateUpdateUser } = require("../middlewares/validator");
 const {
   getUserById,
   updateUser,
 } = require("../controllers/user.controller.js");
 
 router.get("/", verifyToken, getUserById);
-router.put("/", verifyToken, updateUser);
+router.put("/", verifyToken, validateUpdateUser, updateUser);
 
 module.exports = router;
