d13e6c637d53f6ad7f6acca832b42d7972defc21
Faza Dandy <fazadandyakmalin0@gmail.com>
2024-08-02 10:52:39 +0700

    add dialect module

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
 const path = require("path");
+import mysql2 from "mysql2";
 
 require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 
 module.exports = {
   development: {
     dialect: "mysql",
+    dialectModule: mysql2,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USER,
@@ -13,6 +15,7 @@ module.exports = {
   },
   production: {
     dialect: "mysql",
+    dialectModule: mysql2,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USER,
